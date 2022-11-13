a47eb1515260cadd193cecff478507905f40562d
Christian Kjær <ckl@codetalk.io>
2022-11-13 17:17:15 +0100

    Update all actions

---
+++ devenv.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.haskell.enable = true;
+  languages.ruby.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+}
