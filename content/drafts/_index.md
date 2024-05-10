d8abcc56ac3f6018ba02796271c367c054c3735a
Christian Kjær <ckl@codetalk.io>
2024-05-10 15:15:25 +0200

    Set up Zola

---
+++ content/drafts/_index.md
@@ -0,0 +1,6 @@
++++
+title = "List of draft posts"
+sort_by = "date"
+template = "blog.html"
+page_template = "blog-page.html"
++++
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    files: ["./templates/**/*.html", "./theme/**/*.html"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
