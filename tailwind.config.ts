08c7c12fe039797b90e338b10172bf0ae9d9cb5d
Christian Kjær <ckl@codetalk.io>
2024-05-10 15:54:11 +0200

    Configure tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,39 @@ module.exports = {
     files: ["./templates/**/*.html", "./theme/**/*.html"],
   },
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#F7F7F8",
+    },
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#F7F7F8",
+        brand: {
+          50: "#FFF7EB",
+          100: "#FFE8C6",
+          200: "#FFCF88",
+          300: "#FFAB40", // Primary
+          400: "#FF9320",
+          500: "#F96D07",
+          600: "#DD4A02",
+          700: "#B72F06",
+          800: "#94230C",
+          900: "#7A1F0D",
+          950: "#460C02",
+        },
+        dark: {
+          background: "#0E1116",
+          text: "#F7F7F8",
+        },
+        light: {
+          background: "#F7F7F8",
+          text: "#3A373E",
+        },
+      },
+    },
   },
   plugins: [],
 };
