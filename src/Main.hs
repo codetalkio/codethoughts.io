24894828a49ed2b244a2e13835f539d1e1594602
Christian Kjær <ckl@codetalk.io>
2024-06-25 16:25:24 +0200

    Add importable posts

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,9 @@ postPatterns = "posts/*.md" .||. "posts/*.html"
 draftsPatterns :: Pattern
 draftsPatterns = "drafts/*.md" .||. "drafts/*.html"
 
+importablePatterns :: Pattern
+importablePatterns = "importable/*.md" .||. "importable/*.html"
+
 -- | Define the rules for the site/hakyll compiler
 main :: IO ()
 main = hakyll $ do
@@ -116,6 +119,20 @@ main = hakyll $ do
 
   -- | Drafts: The draft posts as an HTML page
   match draftsPatterns $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      -- Create snapshots of the raw post, before any templates are applied. We can get this
+      -- later by loading the snapshot from `raw-post-content`.
+      >>= saveSnapshot "raw-post-content"
+      >>= loadAndApplyTemplate "templates/post.html"    (postCtx tags)
+      -- Create snapshots of the processed post, before any the final templates is applied. We can get this
+      -- later by loading the snapshot from `postContent`.
+      >>= saveSnapshot "processed-post-content"
+      >>= loadAndApplyTemplate "templates/default.html" (postCtx tags)
+      >>= relativizeUrls
+
+  -- | Importable: The posts in a format that's importable by other sites
+  match importablePatterns $ do
     route   $ setExtension "html"
     compile $ pandocCompiler
       -- Create snapshots of the raw post, before any templates are applied. We can get this
